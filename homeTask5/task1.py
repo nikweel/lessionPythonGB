300cf0c493a1d765ce26ef2c9e2f793a192fa53b
Николай Федотов <nikweel@nikweel.local>
2022-10-27 18:45:47 +0200

    add 5 home task

---
+++ homeTask5/task1.py
@@ -0,0 +1,10 @@
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"
+
+my_text = 'Тест текст абв тест'
+
+def del_some_words(my_text):
+    my_text = list(filter(lambda x: 'абв' not in x, my_text.split()))
+    return " ".join(my_text)
+
+my_text = del_some_words(my_text)
+print(my_text)
